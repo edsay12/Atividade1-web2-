1da8aaf8c14e71948489f078c7ba097aa0636e56
edsay12 <edvandearaujo2@hotmail.com>
2022-04-09 09:20:05 -0300

    mudança no print

--- mult/app.js
+++ mult/app.js
@@ -9,7 +9,7 @@ app.get('/mult', function(req, res) {
     if(!opt1 || !opt2) return res.status(400).send({mult:'Erro ou falta de parametros'})
     var mult = opt1 * opt2
     res.status(200).send({mult}) // retorna a multiplicação 
-    console.log('ta na mão')
+    console.log('funcionaou')
    });
 
 app.listen(5000,()=>{
